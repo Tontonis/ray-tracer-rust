51a2ff52e3d0d5f7450835a822a2296b9dff0173
Tontonis <humpfgo@gmail.com>
2025-12-26 21:28:29 +0000

    Add inline progress indicator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
             let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -21,4 +22,5 @@ fn main() {
             print!("{} {} {}\n", ir, ig, ib);
         }
     }
+    eprint!("\nDone\n");
 }
