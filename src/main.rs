28d80121812b4da925e595c9f5973ec4306415b3
Tontonis <humpfgo@gmail.com>
2025-12-26 21:23:53 +0000

    Implement simple gradient image output. Add image outputs to gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // Make image
+
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    // Render
+
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b: f64 = 0.25;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
 }
